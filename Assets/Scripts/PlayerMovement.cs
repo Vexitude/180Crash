a9183a2696382ffdd46517090d92cca772f00f3f
AlanBuell <frog13cha1r@gmail.com>
2024-10-31 14:45:19 -0700

    Added player controls to player script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * Name: Alan Buell
+ * Date: 10/31/2024
+ * Des: Player inputs, movement, lives.
+ */
 public class PlayerMovement : MonoBehaviour
 {
+    public float moveSpeed = 10;
+    public float jumpForce = 10;
+    public float deathY = -5f;
+
+    public int lives = 3;
+
+    private Vector3 moveDir;
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveDir = Vector3.left;
+
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDir = Vector3.right;
+
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveDir = Vector3.forward;
+
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDir = Vector3.back;
+
+            rb.MovePosition(transform.position + moveDir * moveSpeed * Time.deltaTime);
+        }
+
     }
+
 }
